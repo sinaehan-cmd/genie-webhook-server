78aa9c699c043ae70be8ddcb70b8de3d7dc4bccb
sinaehan-cmd <seseragi.sh@gmail.com>
2025-10-19 17:06:23 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Genie Webhook Server is running!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print("📩 Received Webhook:", data)
+    return jsonify({"status": "success", "received": data}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
